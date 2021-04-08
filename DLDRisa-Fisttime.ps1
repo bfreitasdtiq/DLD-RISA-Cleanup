4b05569d1bf70f05b1d05e69a78996bc7a448053
bfreitasdtiq <46353568+bfreitasdtiq@users.noreply.github.com>
2021-04-07 22:58:08 -0700

    Update DLDRisa-Fisttime.ps1

--- DLDRisa-Fisttime.ps1
+++ DLDRisa-Fisttime.ps1
@@ -1,5 +1,5 @@
 New-EventLog -LogName DTIQ-RemoteMan -Source DTIQ
 sc create "DTIQ-DLDRISACleanup" Displayname= "DTIQ-DLDRISACleanup" binpath= "C:\DTIQ\DTIQ-DLD-RISACLEANUP.EXE" start= auto 
-Write-EventLog -log DTIQ-Remoteman -source DTIQ -EntryType Information -eventID 201 -Message "Risa Drive Cleanup Service has been installed"
 Remove-Item -Path \Settings\DLDRisa-Fisttime.ps1
 Start-Service -Name "DTIQ-DLDRISACleanup"
+Write-EventLog -log DTIQ-Remoteman -source DTIQ -EntryType Information -eventID 201 -Message "Risa Drive Cleanup Service has been installed"
